2a4ec56ea76b8c3a5e80e1055f180a09389fb168
Nuo <2656256179@qq.com>
2025-08-20 15:31:34 +0800

    Site updated: 2025-08-20 15:31:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/19/hello-world/"];function toRandomPost(){
+var posts=["2025/08/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
