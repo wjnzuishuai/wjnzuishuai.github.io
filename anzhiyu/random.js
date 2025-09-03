6c6dd4a27187306ea6166566cbf920f6e3b02238
Nuo <2656256179@qq.com>
2025-09-04 00:00:08 +0800

    Site updated: 2025-09-04 00:00:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/20/hello-world/"];function toRandomPost(){
+var posts=["2025/08/20/hello-world/","2025/09/04/OnePlus13ROOT/","2025/09/04/在Vscode中写markdown文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
